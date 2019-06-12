527bd58c296c98f3aef9d5e3f8ed1f6b02dc9481
Pasha Pourmand <Pasha_Pourmand@symantec.com>
2019-06-11 18:19:12 -0700

    finish feature for tap to change sprites

--- PokedexViewController.swift
+++ PokedexViewController.swift
@@ -18,12 +18,25 @@ class PokedexViewController: UIViewController {
     @IBOutlet weak var pokemonSpriteImageView: UIImageView!
     
     var currentPokemon: Pokemon?
+    var currentSpriteIndex: Int = 0
     var statViewController: PokemonStatsTableViewController?
+    
+    @IBAction func tappedSprite(_ sender: Any) {
+        
+        if self.currentSpriteIndex == 3 {
+            self.currentSpriteIndex = 0
+        }
+        else {
+            self.currentSpriteIndex += 1
+        }
+        
+        self.downloadAndDisplaySprite(pokemonSpriteUrl: (currentPokemon?.sprites.set[self.currentSpriteIndex])!)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        PokeApi.sharedInstance.getPokemonData(pokemonName: "magikarp", completionHandler: {(success, pokemon) in
+        PokeApi.sharedInstance.getPokemonData(pokemonName: "hitmonlee", completionHandler: {(success, pokemon) in
             if (!success) {
                 print("Error calling API")
             }
@@ -45,26 +58,12 @@ class PokedexViewController: UIViewController {
                     self.pokemonTypeTwoLabel.text = ""
                 }
                 
-                for (name, stat) in pokemon.stats {
-                    print("\(name): \(stat.baseStat)")
-                }
-                
-                
                 self.statViewController?.setStats(attackStat: pokemon.stats["attack"]!.baseStat, healthStat: pokemon.stats["hp"]!.baseStat, defenseStat: pokemon.stats["defense"]!.baseStat, specialAttackStat: pokemon.stats["special-attack"]!.baseStat, specialDefenseStat: pokemon.stats["special-defense"]!.baseStat, speedStat: pokemon.stats["speed"]!.baseStat)
                 
                 // downloads the sprite and displays it in the imageview
-                let frontSpriteUrl = URL(string: pokemon.sprites.frontDefault)!
-                URLSession.shared.dataTask(with: frontSpriteUrl as URL, completionHandler: { (data, response, error) -> Void in
-                    if error != nil {
-                        print(error ?? "No Error")
-                        return
-                    }
-                    DispatchQueue.main.async(execute: { () -> Void in
-                        let image = UIImage(data: data!)
-                        self.pokemonSpriteImageView.image = image
-                    })
-                    
-                }).resume()
+                self.downloadAndDisplaySprite(pokemonSpriteUrl: pokemon.sprites.frontDefault)
+                
+                print(self.currentPokemon?.typeEffectiveness)
             }
         })
     }
@@ -74,4 +73,23 @@ class PokedexViewController: UIViewController {
             self.statViewController = segue.destination as? PokemonStatsTableViewController
         }
     }
+    
+    func downloadAndDisplaySprite(pokemonSpriteUrl: String) {
+        if !pokemonSpriteUrl.isEmpty {
+            let frontSpriteUrl = URL(string: pokemonSpriteUrl)!
+            URLSession.shared.dataTask(with: frontSpriteUrl as URL, completionHandler: { (data, response, error) -> Void in
+                if error != nil {
+                    print(error ?? "No Error")
+                    return
+                }
+                DispatchQueue.main.async(execute: { () -> Void in
+                    let image = UIImage(data: data!)
+                    self.pokemonSpriteImageView.image = image
+                })
+                
+            }).resume()
+        }
+    }
 }
+
+
